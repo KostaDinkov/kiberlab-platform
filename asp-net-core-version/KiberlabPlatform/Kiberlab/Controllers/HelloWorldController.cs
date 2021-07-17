8040745ed97c0b6ade91ebebe904551c9ed3245d
Kosta <k.v.dinkov@gmail.com>
2021-07-17 14:20:35 +0300

    project update

---
+++ asp-net-core-version/KiberlabPlatform/Kiberlab/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Kiberlab.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            this.ViewData["Message"] = "Hello " + name;
+            this.ViewData["NumTimes"] = numTimes;
+            
+            return this.View();
+        }
+
+
+    }
+}
